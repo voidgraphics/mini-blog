c6f97bd5f58f67d75477487649037cdcc8f97c92
Adrien Leloup <nittiyh@outlook.com>
2015-03-03 11:36:34 +0100

    Added user collect form

---
+++ views/collect_user.php
@@ -0,0 +1,13 @@
+<form action="" method="POST">
+    <div>
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" id="username" name="username"/>
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" name="password"/>
+
+        <input type="hidden" name="a" value="check"/>
+        <input type="hidden" name="e" value="user"/>
+
+        <input type="submit"/>
+    </div>
+</form>
