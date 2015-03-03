a9ac1de2bb43104b2e08143de4110b0cd349e388
Adrien Leloup <nittiyh@outlook.com>
2015-03-03 11:28:24 +0100

    Added users controller

---
+++ models/M_Users.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class M_Users extends Model
+{
+    public function getUser($username, $password)
+    {
+        $sql = 'SELECT $ FROM users WHERE username=:username AND password=:password;';
+        $pdost = $this->connexion->prepare($sql);
+        $pdost->execute([':username'=>$username, ':password'=>$password]);
+        return $pdost->fetch();
+    }
+
+    public function createUser($username, $password)
+    {
+        $sql = 'INSERT INTO users(username, password) VALUES(:username, :password);';
+        $pdost = $this->connexion->prepare($sql);
+        $pdost->execute([':username' => $username, ':password' => $password]);
+    }
+}
