6d19db23c2de7fea1db372d778c81ade9e4f25cc
Adrien Leloup <nittiyh@outlook.com>
2015-03-03 13:02:29 +0100

    Corrected hidden value

--- views/collect_user.php
+++ views/collect_user.php
@@ -1,4 +1,4 @@
-<form action="" method="POST">
+<form action="index.php?a=check&e=users" method="POST">
     <div>
         <label for="username">Nom d'utilisateur</label>
         <input type="text" id="username" name="username"/>
@@ -6,7 +6,7 @@
         <input type="password" id="password" name="password"/>
 
         <input type="hidden" name="a" value="check"/>
-        <input type="hidden" name="e" value="user"/>
+        <input type="hidden" name="e" value="users"/>
 
         <input type="submit"/>
     </div>
