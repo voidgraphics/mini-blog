9af9e659cd6bc286acff0310b1fc28a4cc194022
Adrien Leloup <nittiyh@outlook.com>
2015-03-03 11:56:46 +0100

    Added user routes

--- configs/routes.php
+++ configs/routes.php
@@ -8,3 +8,6 @@ $routes['create_posts'] = 'create/posts';
 $routes['update_posts'] = 'update/posts';
 $routes['delete_posts'] = 'delete/posts';
 $routes['view_error'] = 'view/error';
+$routes['collect_user'] = 'collect/user';
+$routes['check_user'] = 'check/user';
+$routes['disconnect_user'] = 'disconnect/user';
